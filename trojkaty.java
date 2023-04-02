e8e5a6909060feeb172b4b8cd3d3ff364b10492f
kbazgier@student.wszib.edu.pl <kbazgier@student.wszib.edu.pl>
2023-04-02 17:30:01 +0200

    dodanie nowych zaleznossci do programy rozponajacego trojkaty

--- trojkaty.java
+++ trojkaty.java
@@ -10,15 +10,38 @@ class Trojkaty {
      * @param {float} c - Długość trzeciego boku.
      */
     public static void jakiTrojkat(float a, float b, float c){
+
+    if(a+b > c && a+c > b && b+c > a){
+    
          if (a == b && b == c && a == c) {
-             System.out.println("Trójkąt równoboczny"); 
+             System.out.print("Trójkąt równoboczny"); 
          }
-         // TODO: tutaj trzeba bedzie dopisac inne przypadki
+         else if (a==b || b==c || c==a){
+            System.out.print("Trojkat rownoramienny");
+         }
+         else if (a != b && b != c && c != a){
+            System.out.print("Trojkat roznoboczny");
+         }
+        
+         if ((a*a + b*b)== c*c){
+            System.out.println(",  prostokatny");
+         }
+         else if((a*a + b*b) < c*c){
+            System.out.println(", rozwartokatny");
+         }
+         else if((a*a + b*b) > c*c){
+            System.out.println(", ostrokatny");
+         }
+     }
+     else{
+        System.out.println("Nie moze byc trojkatem");
+     }
+         
     }
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
-        System.out.println("Program do rozpoznawania rodzaju trójkąra");
+        System.out.println("Program do rozpoznawania rodzaju trójkąta");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
     /** Glowna funkcja */
