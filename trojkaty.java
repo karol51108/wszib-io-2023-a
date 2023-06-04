4af83c1d9c13c7a4effa9cba602a50cf58d029ee
Maciej Jarosław Jachym <mjachym@artemis.wszib.edu.pl>
2023-06-04 18:11:04 +0200

    poprawa komentarza

--- trojkaty.java
+++ trojkaty.java
@@ -46,7 +46,7 @@ class Trojkaty {
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
         System.out.println("Program do rozpoznawania rodzaju trójkąta");
-        System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
+        System.out.println("Uruchom z trzema argumentami liczbowymi - długości boków trójkąta");
     }
     /** Glowna funkcja */
     public static void main(String... args) {
